efd6cc07ee74a16d600563973d8fc66fd71714c2
Jerome Manarang <jemanara@uci.edu>
2025-03-04 23:31:51 -0800

    fixed tailwind import

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Home = ({onStart}) => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
-            <h1 className="text-4xl font-bold">Before You Knew</h1>
+        <div className="flex flex-col items-center justify-center h-screen bg-white text-purple text-center">
+            <h1 className="text-4xl font-bold text-purple ">Before You Knew</h1>
             <button
                 onClick={onStart}
                 className="mt-6 px-6 py-3 bg-white text-black rounded-full shadow-lg transition hover:scale-110"
